972823574accc5fe3a11c13560436580418e9133
BranalEc <20240208@ricaldone.edu.sv>
2025-07-28 12:13:07 -0600

    Tercer medio Avance

--- src/main/java/BrandonOrellana_20240208/BrandonOrellana_20240208/Services/ProveedorService.java
+++ src/main/java/BrandonOrellana_20240208/BrandonOrellana_20240208/Services/ProveedorService.java
@@ -1,4 +1,16 @@
 package BrandonOrellana_20240208.BrandonOrellana_20240208.Services;
 
+import BrandonOrellana_20240208.BrandonOrellana_20240208.Repositories.ProveedorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ProveedorService {
+
+    @Autowired
+    private ProveedorService acceso;
+
+
 }
